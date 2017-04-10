890fce30bedabb47af9436abe53cdf08ab45b355
teenstitan <ljrfoxteenstitan@gmail.com>
2017-04-10 15:29:34 +0800

    Initial commit

---
+++ citypicker/src/main/java/com/ljstudio/android/citypicker/utils/JAssetsUtils.java
@@ -0,0 +1,38 @@
+package com.ljstudio.android.citypicker.utils;
+
+import android.content.Context;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class JAssetsUtils {
+    
+    /**
+     * 读取Assets目录下面指定文件并返回String数据
+     * @param context
+     * @param fileName
+     * @return
+     */
+    public static String getJsonDataFromAssets(Context context, String fileName) {
+        StringBuilder stringBuilder = new StringBuilder();
+        InputStream inputStream = context.getClass().getClassLoader().getResourceAsStream("assets/" + fileName);
+        try {
+            byte[] buffer = new byte[inputStream.available()];
+            inputStream.read(buffer);
+            String json = new String(buffer, "utf-8");
+            stringBuilder = stringBuilder.append(json);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                inputStream.close();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
---
+++ citypicker/src/main/java/com/ljstudio/android/citypicker/widget/CanShow.java
@@ -0,0 +1,12 @@
+package com.ljstudio.android.citypicker.widget;
+
+
+public interface CanShow {
+    void setType(int var1);
+
+    void show();
+
+    void hide();
+
+    boolean isShow();
+}
